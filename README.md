3056154d015e4a8e6d4f5029b61da6bb258fd018
Николай Проворов <nikolajprovorov@deboshirikk-2.local>
2025-01-14 15:59:07 +0300

    bug fix

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Polish-cow
+# BIG THANKS TO edelwiw
+
+This is a simple Arduino project that can make a cow move and sing a strange Polish song. To repeat this, you will need :
+1. Arduino pro mini because it small
+2. Amplifier
+3. Speaker 28mm in diameter
+4. Button
+5. Servo SG 90/92R and others with the same dimensions
+6. BMS TP4056 Type-c 
+7. Li-Ion battery (optional)
+
+# Connect components to arduino
+
+You should attach servo to 8 digital pin, amplifier to 9 digital pin, button to 5 pin. If you want to add battery you can connect transistor to 7 pin.
+
+# Connect components to BMS
+
+## Without battery
+
+## With battery
+
--- polish_cow.ino
+++ polish_cow.ino
@@ -8,27 +8,22 @@
 #define LA 440
 #define T 250
 #define BUTTON_PIN 5
+#define PIN_TONE 9
 
 #include <Servo.h>
-// #include <GyverTimers.h>
-
-
-uint32_t timer_btn = 0;
-
 
 Servo myservo;
 void setup() {
-  // put your setup code here, to run once:
-   // номер порта зуммера
-  const uint8_t COUNT_NOTES = 39; // Колличество нот
   myservo.attach(8);
   myservo.write(90);
   pinMode(BUTTON_PIN, INPUT);
-  pinMode(13, OUTPUT);
   pinMode(7, OUTPUT);
   digitalWrite(7, 0);
+  delay(50);
+  myservo.detach();
 }
 
+// int frequency[] = {MI, LA, LA, MI, MI, DO, MI, MI, MI, MI, DO, RE, RE, RE, RE};
 int frequency[] = {MI, LA, LA, MI, MI, DO, MI, MI, MI, MI, DO, RE, RE, RE, RE};
 int duration[] = {
   T, T, T, T, T, T*3,
@@ -36,28 +31,28 @@ int duration[] = {
   50, T*2, T, T*2, T*3
 };
 
-uint8_t Pin_tone = 9;
-
 int pos[] = {
   45, 65, 45, 135, 115, 135 
 };
 
-bool s = 0; 
 int ind = 0;
 bool btn_state = 0;
 uint8_t duration_serv = 250;
+uint32_t timer_btn = 0;
 
 
 void move_cow(){
+  myservo.attach(8);
   digitalWrite(7, 1);
+  delay(200);
   uint32_t timer = 0;
   uint32_t timer_serv = 0;  
   int ind_serv = 0;
   int bebra = 0;
   int bober = 2;
-  while (bober > 0){
+  while (bober >= 0){
     if (millis() - timer > bebra) {
-      tone(Pin_tone, frequency[ind], duration[ind]);
+      tone(PIN_TONE, frequency[ind], duration[ind]);
       bebra = duration[ind];
       ind = (ind + 1) % (sizeof(frequency)/sizeof(int));
       if (sizeof(frequency) / sizeof(int) - ind == 1){
@@ -70,10 +65,18 @@ void move_cow(){
       timer_serv = millis();
       myservo.write(pos[ind_serv]);
       ind_serv = (ind_serv + 1) % (sizeof(pos)/sizeof(int));
-    } 
+    }
+    
+    if (bober == 0 && ind == 6) {
+      ind = 0;
+      break; 
+    }
   }
   while(millis() - timer < bebra) {};
   digitalWrite(7, 0);
+  myservo.write(90);
+  delay(50);
+  myservo.detach();
 }
 
 void loop() {
